f7b56d816982942ca30e778f665ad7f48b9044ab
Andres Niño <andresalejandroninoaraujo@gmail.com>
2026-07-26 14:55:14 -0500

    prueba de directorio como input

--- arreglos.py
+++ arreglos.py
@@ -1,24 +1,43 @@
 from pathlib import Path
-from docling.document_converter import DocumentConverter
+from docling.document_converter import DocumentConverter,InputFormat
 
-source = "/home/anothercoolcoder/principito.pdf"  # archivo o enlace
+input_dir = Path("./documentos_entrada")
+output_dir = Path(".documentos_salida")
+
+output_dir.mkdir(parents=True, exist_ok=True)
 
 converter = DocumentConverter()
-result = converter.convert(source)
-doc = result.document
 
-parrafos = [item.text for item in doc.texts]
+extensiones_soportadas = {
+    ext 
+    for fmt in InputFormat
+    for ext in fmt.supported_file_extensions
+}
 
-tablas = []
+archivos = [
+    f for f in input_dir.iterdir()
+    if f.is_file() and f.suffix.lower() in extensiones_soportadas
+]
 
-for table in doc.tables:
-    df = table.export_to_dataframe()
-    tablas.append(df.values.tolist())
+print(f"Se encontraron {len(archivos)} archivos compatibles con Docling.\n")
 
-print(f"Total de párrafos extraídos: {len(parrafos)}")
-print("\nPrimeros 3 párrafos:")
-print(parrafos[:3])
 
-if tablas:
-    print("\nPrimera tabla extraída como arreglo de filas:")
-    print(tablas[0])
+# converter = DocumentConverter()
+# result = converter.convert(source)
+# doc = result.document
+
+# parrafos = [item.text for item in doc.texts]
+
+# tablas = []
+
+# for table in doc.tables:
+    # df = table.export_to_dataframe()
+    # tablas.append(df.values.tolist())
+
+# print(f"Total de párrafos extraídos: {len(parrafos)}")
+# print("\nPrimeros 3 párrafos:")
+# print(parrafos[:3])
+
+# if tablas:
+    # print("\nPrimera tabla extraída como arreglo de filas:")
+    # print(tablas[0])
